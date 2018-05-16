85d575dff9b5e6523e264ae7f6d21fa0df007d6c
Rosie <rodupontayala@ucdavis.edu>
2018-05-16 12:58:40 -0700

    added UpdateNeighboringCells method to class

--- influence.py
+++ influence.py
@@ -1,4 +1,4 @@
-import numpy
+import math
 from our_class_files import vOneZergling
 
 from pysc2.agents import base_agent
@@ -29,10 +29,11 @@ class Influence:
     
     reaper = {'speed': 5.25,'acceleration' : 1000, 'deceleration': 0, 'attackTime': .79, 'turnTime': 0, 'kitingTime': 0}
     I_Map = [[0 for x in range(32)] for y in range(32)] # influence map
+    unitFeaturesDict = {} 
+    unitDmaxDict = {}
     
     # Here is the feature dictionary that contains the features for each unit
     # The structure of each item in the dictionary is : 'name_of_unit' : UnitFeatures(attackRange, speedOffCreep, speedOnCreep, dps)
-    unitFeaturesDict = {} 
     unitFeaturesDict['Zergling'] = UnitFeatures(0.1, 4.13, 6.58, 100)
     unitFeaturesDict['ZerglingSpeedBoost'] = UnitFeatures(0.1, 5.37, 8.55, 10)    
     unitFeaturesDict['Roach'] =  UnitFeatures(, , ,)
@@ -41,7 +42,7 @@ class Influence:
     unitFeaturesDict['Drone'] = UnitFeatures( , , ,)
 
     # unitDmaxDict is the dictionary that contains the oncreepdmax and offcreepdmax for each unit
-    unitDmaxDict = {}
+    
     for name, unitFeatures in unitFeaturesDict:
       offdmax =  unitFeatures.attackRange + 1 + unitFeatures.speedOffCreep + self[reaper]['kitingTime']
       ondmax =  unitFeatures.attackRange + 1 + unitFeatures.speedOnCreep + self[reaper]['kitingTime']
@@ -55,9 +56,23 @@ class Influence:
       for name,creepAndLocation in enemyDict:
       
         if creepAndLocation['creep']:
-          dmax = unitDmaxArray[name]['oncreepdmax']
+          dmax = unitDmaxDict[name]['oncreepdmax']
         else:
-          dmax = unitDmaxArray[name]['offcreepdmax']
+          dmax = unitDmaxDict[name]['offcreepdmax']
         
-      	reaperOutRuns = false
-        #update cell with DPS determined by dMax
+        enemyPosition = creepAndLocation['position']
+      	self.updateNeighboringCells(dmax, enemyPosition, name)
+
+
+
+    def updateNeighboringCells(dmax, enemyPosition, name):
+
+      x = enemyPosition.x
+      y = enemyPosition.y
+      for dy in range(-1,2):
+        for dx in range(-1,2):
+          distance = math.sqrt(dx ** 2 + dy ** 2)
+          if distance <= dmax:
+            self.I_Map[enemyPosition.x + dx][enemyPosition.y + dy] = unitFeaturesDict[name].dps
+          else:
+            self.I_Map[enemyPosition.x + dx][enemyPosition.y + dy] = 0
