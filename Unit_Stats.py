6dd74c97a0865992478429f03db6f85b8bdaa20f
Daniel Loran <dcloran@ucdavis.edu>
2018-05-23 09:06:49 -0700

    updated Unit_Stats.py with canKite() functionality

--- Unit_Stats.py
+++ Unit_Stats.py
@@ -3,9 +3,20 @@
 #Dmax
 #canKite
 #order of stats: speed, acceleration, deceleration, turn, windUp, windDown, attackRange, HP, DPS,
-
+import math
 
 class UnitStats:
-    reaperStats = {'speed' : 3.75, 'acceleration' : 1000, 'deceleration' : 0, 'turnRate' : 999.8437, 'windUp' : 1.1, 'windDown' : .75, 'attackRange' : 5, 'HP' : 60, 'DPS' : 7.27}
+    reaper = {'speed' : 3.75, 'acceleration' : 1000, 'deceleration' : 0, 'turnRate' : 999.8437, 'windUp' : 1.1, 'windDown' : .75, 'attackRange' : 5, 'HP' : 60, 'DPS' : 7.27, 'radius' : 0.375}
     enemyStats = {'zergling' : zergling}
-    zergling = {'speed' : 2.95, 'creepSpeed' : 3.84, 'acceleration' : 1000, 'deceleration' : 0, 'turnRate' : 999.8437, 'windUp' : 0.696, 'windDown' : .5, 'attackRange' : 0.1, 'HP' : 35, 'DPS' : 7.14}
+    zergling = {'speed' : 2.95, 'creepSpeed' : 3.84, 'acceleration' : 1000, 'deceleration' : 0, 'turnRate' : 999.8437, 'windUp' : 0.696, 'windDown' : .5, 'attackRange' : 0.1, 'HP' : 35, 'DPS' : 7.14, 'radius' : 0.375}
+
+def kitingTime():
+
+    return reaper['windUp'] + (1/reaper['acceleration']) + (2 * math.pi * reaper['radius'] * (1/reaper['turnRate']))
+
+def canKite(target):
+
+    reaperFaster = reaper['speed'] > enemyStats[target]['speed']
+    reaperOutrange = reaper['attackRange'] > enemyStats[target]['attackRange'] + (enemyStats[target][speed] * kitingTime())
+
+def dMax(target)
