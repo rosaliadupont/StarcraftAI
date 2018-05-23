9e03af9e9387f9715ac89796f0ac7b83036bda31
Daniel Loran <dcloran@ucdavis.edu>
2018-05-23 11:48:16 -0700

    changed function names from camelCasing to underscores, which is the standard for python. The More You Know!

--- Unit_Stats.py
+++ Unit_Stats.py
@@ -1,6 +1,5 @@
-#Rename to UnitStats.py for consistency
 #Internal Functions - see Kiting-RTS-Influence-Maps.pdf for definitions of these Functions
-#Dmax
+#dMax
 #canKite
 #order of stats: speed, acceleration, deceleration, turn, windUp, windDown, attackRange, HP, DPS,
 import math
@@ -10,16 +9,17 @@ class UnitStats:
     enemyStats = {'Zergling' : Zergling}
     Zergling = {'speed' : 2.95, 'creepSpeed' : 3.84, 'acceleration' : 1000, 'deceleration' : 0, 'turnRate' : 999.8437, 'windUp' : 0.696, 'windDown' : .5, 'attackRange' : 0.1, 'HP' : 35, 'DPS' : 7.14}
 
-def kitingTime():
+def kiting_time():
 
     return reaper['windUp'] + (1/reaper['acceleration']) + (2 * math.pi  * (1/reaper['turnRate']))
 
-def canKite(target):
+
+def can_kite(target):
 
     reaperFaster = Reaper['speed'] > enemyStats[target]['speed']
     reaperOutrange = Reaper['attackRange'] > enemyStats[target]['attackRange'] + (enemyStats[target][speed] * kitingTime())
 
-def dMax(target):
+def d_max(target):
 
 	k = 1; #confidence constant value, can be adjusted
 	return enemyStats[target]['attackRange'] + k + (enemyStats[target][speed] * kitingTime())
