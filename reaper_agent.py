16f220e901777c44f683b432b88ddce9b81e402b
theacalix <emilyhery@gmail.com>
2018-05-25 16:45:29 -0700

    made influence class, modified influence map with new enemy class, expanded reaper_agent code to include obs

--- reaper_agent.py
+++ reaper_agent.py
@@ -1,45 +1,65 @@
-'''tick() {
-    target = targetSelection();
-    if (canKite(target)) {
-        kitingAttack(target);
-    } else {
-        attack(target);
-    }
-}
-kitingAttack(target) {
-    position = getSecurePosition(actualPos);
-    if (position == actualPos) {
-        attack(target);
-    } else {
-        move (position); //fleemovement
-    }
-}'''
-
 from pysc2.agents import base_agent
 from pysc2.lib import actions
 from pysc2.lib import features
 
+#unit ids
+_REAPER = 49
+
 class ReaperAgent(base_agent.BaseAgent):
-    def __init__():
+    enemy_array = []
+    reaper_array = []
+    unit_stats
+    i_map
+    control_not_set = True
+    not_centered = True
+
+    def __init__(self):
         #creates enemy array and unitstats and influence map
+        #super function class super class init
+        unit_stats = UnitStats()
+        i_map = InfluenceMap()
 
     def step(self, obs):
-        update_obs()
-        target = select_target()
+        if control_not_set:
+            action = set_control_groups(obs)
+            control_not_set = False
 
-        if can_kite(target): #method in unittype class
-            kiting_attack(target)
+        elif not_centered:
+            action = select_n_center()
+            not_centered = False #TODO: check to see if still centered
         else:
-            #what do we do in the else case?
-            #if low health,run
-            #else attack
-            #check if you can kill them faster than they can
-            #cant run, fight until death
+            #main loop
+            update_obs() #assumes screen is already in position
+            target = select_target()
+
+            if can_kite(target): #method in unittype class
+                action = kiting_attack(target)
+            else:
+                #what do we do in the else case?
+                #if low health,run
+                #else attack
+                #check if you can kill them faster than they can
+                #cant run, fight until death
+                #search for enemies
+
+        return action
+
+    def set_control_groups(obs):
 
     def update_obs():
         #fills enemy array
         #calls dmax
         #calls update map
+        for unit = obs.observation.feature_units:
+            if unit.alliance == _HOSTILE_ALLIANCE:
+                #store relevent data in Enemy class
+                new_enemy = Enemy(unit.health, [unit.x, unit.y], unit.unit_type, unit_stats.d_max(unit.unit_type))
+                enemy_array.append(new_enemy)
+            elif unit.type == _REAPER:
+                #update reaper in reaper array. We only have one reaper rn
+                reaper_array[0].update(unit.health, [unit.x, unit.y], unit.is_selected)
+
+        i_map.update_map(enemy_array)
 
     def select_target():
         #created by thomas
@@ -48,9 +68,9 @@ class ReaperAgent(base_agent.BaseAgent):
     def kiting_attack(target):
         position = get_secure_pos(actual_pos)
         if position == actual_pos:
-            attack(target)
+            return attack(target)
         else:
-            move(position)
+            return move(position)
 
     def attack(target):
         #sets action to be used by api
