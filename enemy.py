660fcb0ab519f67a662f5e906ab0e3f387e1ba4a
theacalix <emilyhery@gmail.com>
2018-05-27 22:40:51 -0700

    moved variables in class

--- enemy.py
+++ enemy.py
@@ -1,25 +1,18 @@
 
 class Unit:
-    health = 0
-    position = [0, 0] #positon of screen
-    type = ""
     #cooldown
     #facing
     def __init__(self, health, pos, type):
         self.health = health
-        self.position = pos
+        self.position = pos #positon of screen
         self.type = type
 
 class Enemy(Unit):
-    dmax = 0
-
     def __init__(self, health, pos, type, dmax):
         super(Enemy, self).__init__(health, pos, type)
-        self.dmax = dmax 
+        self.dmax = dmax
 
 class Reaper(Unit):
-    is_selected = False
-
     def __init__(self, health, pos, select):
         super(Reaper, self).__init__(health, pos, _REAPER)
         self.is_selected = select
