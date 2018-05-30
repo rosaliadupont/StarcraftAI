ed1362367dcab76389aeb270d0111ee971cf5ba6
Aron Sarmasi <aronsar@gmail.com>
2018-05-30 14:38:26 -0700

    updated select_target to work with sc2, fixed bugs

--- reaper_agent.py
+++ reaper_agent.py
@@ -1,4 +1,6 @@
 import sc2
+from .unit_stats import UnitStats
+from .influenceMap import InfluenceMap
 
 #Weights 3 > 1 > 2
 W1 = 0.75
@@ -18,10 +20,11 @@ class ReaperAgent(sc2.BotAI):
     async def on_step(self, iteration):
 
         update_obs()
-        target = select_target(0)
+        target_obj = enemy_array[select_target(0 '''reaper_index''')]
+        target_name = 'Zergling' # FIXME: should use target_obj.type_id to refer to name string; eg: 84 --> 'Probe'
 
-        if can_kite(target): #method in unittype class
-            action = kiting_attack(target)
+        if UnitStats.can_kite(target_name): #method in unittype class
+            action = kiting_attack(target_obj)
             #we eventually want to do unit.action(), it might be more appropiate to do this in the kiting_attack method though
         else:
             #what do we do in the else case?
@@ -37,34 +40,37 @@ class ReaperAgent(sc2.BotAI):
         #calls update map
         enemy_array = []
         for unit in self.state.units.enemy.not_structure:
-             enemy_array.append([unit.positon, unit_stats.d_max(unit.name)])
+            if unit.is_visible:
+                enemy_array.append([unit.positon, self.unit_stats.d_max(unit.type_id)]) # FIXME: unit.type_id is not a string, it's an int (I think)
 
-        i_map.update_map(enemy_array, unit_stats)
+        self.i_map.update_map(enemy_array, self.unit_stats)
 
-    def select_target(index):
-        #looks at enemy array and returns index of selected unit
+    def select_target(reaper_index):
+        #looks at units in enemy_array[], and returns the index of the appropriate one to attack
         #TODO: convert to using sc2 data
         max_score = 0
-        select_index
+        select_index = -1
 
-        for index, unit in enumerate(enemy_array):
-            d = Distance3D(unit, reaper_array[index])
-            t = unit_stats.enemyStats[unit.type][tactical_threat]
-            a = unit_stats[Reaper][DPS] / (reaper_array[index].health / unit_stats.enemyStats[unit.type][DPS])
+        for i, unit in enumerate(enemy_array):
+            r = reaper_index
+            d = Distance3D(unit, reaper_array[r]) # FIXME: must import Distance3D from somewhere
+            t = self.unit_stats.enemyStats[unit.type][tactical_threat]
+            a = self.unit_stats[Reaper][DPS] / (reaper_array[r].health / self.unit_stats.enemyStats[unit.type][DPS])
 
             targeting_score = （a * W1） + （t * W2） + （d * W3）
 
             if targeting_score > max_score:
                 max_score = targeting_score
-                select_index = index
-            else:
-                continue
-
+                select_index = i
+        
+        if select_index == -1
+            print (f"Error: select_index is -1")
+            
         return select_index
 
-    def kiting_attack(target):
-        position = get_secure_pos(actual_pos)
+    def kiting_attack(target_obj):
+        position = InfluenceMap.get_secure_pos(actual_pos)
         if position == actual_pos:
-            await self.do(reaper_array[0].attack(target))
+            await self.do(reaper_array[0].attack(target_obj))
         else:
             await self.do(reaper_array[0].move(positon))
