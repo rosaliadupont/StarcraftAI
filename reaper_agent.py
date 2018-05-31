005470580f2775abe92fb1f2f2fab50fbb092e7b
Rosie <rodupontayala@ucdavis.edu>
2018-05-30 17:30:48 -0700

    bug fixes in all functions

--- reaper_agent.py
+++ reaper_agent.py
@@ -25,6 +25,7 @@ class ReaperAgent(sc2.BotAI):
             if self.unit_stats.can_kite(target):
                 self.kiting_attack(target, reaper)
             else:
+
                 #what do we do in the else case?
                 #if low health,run
                 #else attack
@@ -32,26 +33,26 @@ class ReaperAgent(sc2.BotAI):
                 #cant run, fight until death
                 #search for enemies
 
-    def update_obs():
+    def update_obs(self):
         #fills enemy array
-        #calls dmax
+        #calls d_max
         #calls update map
         self.enemy_array = []
         for unit in self.known_enemy_units.not_structure:
-             self.enemy_array.append(Enemy(unit.positon, unit.name, self.unit_stats.d_max(unit.name)])
+             self.enemy_array.append(Enemy(unit.positon, unit.name, self.unit_stats.d_max(unit.name)))
 
-        self.i_map.update_map(enemy_array, self.unit_stats)
+        self.i_map.update_map(self.enemy_array, self.unit_stats)
 
-    def select_target(reaper):
-        #returns postion of most desirable enemy
+    def select_target(self, reaper):
+        #returns position of most desirable enemy
         max_score = 0
 
-        for unit in self.known_ enemy_units.not_structure:
+        for unit in self.known_enemy_units.not_structure: # what is self.known_enemy_units.not_structure?
             d = Distance3D(unit, reaper) #FIXME: where does Distance3D come from?
-            t = self.unit_stats.enemyStats[unit.name][tactical_threat]
-            a = self.unit_stats.Reaper[DPS] / (reaper.health / self.unit_stats.enemyStats[unit.name][DPS])
+            t = self.unit_stats.enemyStats[unit.name]['tactical_threat']
+            a = self.unit_stats.Reaper['DPS'] / (reaper.health / self.unit_stats.enemyStats[unit.name]['DPS'])
 
-            targeting_score = （a * W1） + （t * W2） + （d * W3）
+            targeting_score = (a * W1) + (t * W2) + (d * W3)
 
             if targeting_score > max_score:
                 max_score = targeting_score
@@ -61,9 +62,9 @@ class ReaperAgent(sc2.BotAI):
 
         return target
 
-    def kiting_attack(target, reaper):
+    def kiting_attack(self, target, reaper):
         position = InfluenceMap.get_secure_pos(reaper.positon)
-        if position == actual_pos:
-            await self.do(reaper.attack(target.positon))
+        if position == reaper.position:
+            await self.do(reaper.attack(target.position))
         else:
-            await self.do(reaper.move(positon))
+            await self.do(reaper.move(position))
