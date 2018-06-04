b49a550f05afb1fc1a8530346fc384f088ca3788
Rosalia Dupont <rosaliadupont@campus-059-222.ucdavis.edu>
2018-06-03 19:11:48 -0700

    make influence map same size ad game map size

--- influenceMap.py
+++ influenceMap.py
@@ -11,10 +11,9 @@ import random
 class InfluenceMap:
 
   def __init__(self, size):
-
-    self.I_Map = [[0 for x in range(32)] for y in range(32)] # influence map
-    self.height = size.height
-    self.width = size.width
+	self.height = size.height
+	self.width = size.width
+    self.I_Map = [[0 for x in range(self.width)] for y in range(self.height)] # influence map
     #print(self.I_Map)
     
     #pdb.set_trace()
@@ -37,13 +36,11 @@ class InfluenceMap:
     # entries and checks whether each enemy is within the range
     # of damage, if it is then the entry gets added with this dps
 
-    for x in range(0,32):
-      for y in range(0,32):
+    for x in range(0,self.width):
+      for y in range(0,self.height):
         self.I_Map[x][y] = 0
         for enemy in enemy_array: #[positon, d_max]
-            e_x = int(enemy.x * 32 / self.width) #convert to cell in i_map
-            e_y = int(enemy.y * 32 / self.height)
-            distance = math.sqrt((e_x - x) ** 2 + (e_y - y) ** 2)
+            distance = math.sqrt((enemy.x - x) ** 2 + (enemy.y - y) ** 2)
             if distance <= enemy.d_max:
                 self.I_Map[x][y] += unit_stats.enemyStats[enemy.type]['DPS']
     #pdb.set_trace()
@@ -55,15 +52,15 @@ class InfluenceMap:
     # this function returns a Point3 for the sc2 action to use indicating
     # where the secure closest position is
 
-    r_x = int(actual_position.x * 32 / self.width)
-    r_y = int(actual_position.y * 32 / self.height)
+    r_x = int(actual_position.x)
+    r_y = int(actual_position.y)
 
     if self.I_Map[r_x][r_y] == 0:
       return actual_position
 
     distances_to_coords = {}
-    for x in range(0,32):
-      for y in range(0,32):
+    for x in range(0,self.width):
+      for y in range(0,self.height):
         if self.I_Map[x][y] == 0:
           distance = math.sqrt((r_x - x) ** 2 + (r_y - y) ** 2)
           distances_to_coords[distance] = (x,y)
@@ -73,7 +70,5 @@ class InfluenceMap:
       return -1
 
     map_pos = distances_to_coords[min(distances_to_coords.keys())]
-    x = map_pos[0] * self.width / 32 + self.width / 64 #convert to map size
-    y = map_pos[1] * self.height / 32 + self.height / 64
 
-    return Point2((x, y)) #FIXME: not sure if this works, we need to return a position that the game understands, but idk if you can make a new Point3 object
+    return Point2((map_pos[0], map_pos[1])) #FIXME: not sure if this works, we need to return a position that the game understands, but idk if you can make a new Point3 object
